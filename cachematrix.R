bc588969dc5c73a9efba09d31143a82cfd600e2c
mjy1 <miny@bu.edu>
2015-06-21 01:06:14 -0400

    commit hw2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,46 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# Part 1 - call matrix, get inverse of matrix, cache 
 
+makeCacheMatrix <- function(x = matrix()) {
+  
+  #set up 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  # getting the inverse of matrix and getting it cached with the <<- symbol
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  
+  
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+#Part 2 - return matrix that was rendered before and just printing function here
+
+cacheSolve <- function(matrix(), ...) {
+  
+  # if there is nothing cached - return this part 
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  # there is something cached - return this part
+  matrix <- x$get()
+  m <- solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
